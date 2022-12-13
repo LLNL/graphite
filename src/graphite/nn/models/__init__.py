4491edb129b7f521f4298639c60489495b330adf
Tim Hsu <hsu16@llnl.gov>
2022-12-13 15:19:48 -0800

    minor update

--- src/graphite/nn/models/__init__.py
+++ src/graphite/nn/models/__init__.py
@@ -1,5 +1,7 @@
-from .egconv_gnn import EGCONV_GNN
-from .egcnn import EGCNN
 from .alignn import ALIGNN
 from .alignn_d import ALIGNN_d
-from .alignn_d_interpretable import ALIGNN_d_interpretable
+
+from .egconv_gnn import EGCONV_GNN
+from .egcnn import EGCNN
+
+from .mgn import MeshGraphNet
